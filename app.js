051368bea5581689a6d73054dad80b69d8208d3a
rafaelafae <rafaela.fae@gmail.com>
2024-01-31 08:15:26 -0300

    Final 31/01/2024

--- app.js
+++ app.js
@@ -9,14 +9,26 @@
 
 //Para inserir o texto dentro desta tag, utilizamos .innerHTML, que significa "dentro do HTML";
 
-let titulo = document.querySelector('h1');
+
+//As variáveis abaixo fazem a mesma função, mesmo pegando elementos diferentes para atribuir. Então este código anterior será transformado em função
+/*let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do número secreto';
 
 //No código HTML, na linha 23, temos um parágrafo, representado pela tag "p";
 
 //<p class="texto__paragrafo"></p> (index.html)
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';*/
+
+/*Sempre que temos padrão de código ou um código muito parecido, onde apenas alguns detalhes são modificados, podemos isolar esse comportamento em uma função.
+Transformando a codificação anterior em função, para facilitar e reduzir as linhas do código*/
+function exibirTextoTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+};
+
+exibirTextoTela('h1', 'Jogo do número secreto');
+exibirTextoTela('p', 'Escolha um número entre 1 e 10');
 
 //Habilitar botão da linha 27: <button onclick="verificarChute()" class="container__botao">Chutar</button>
 
