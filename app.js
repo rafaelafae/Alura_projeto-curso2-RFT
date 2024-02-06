c268b2eba651206e7cafb496c0bd184e02e97d9f
rafaelafae <rafaela.fae@gmail.com>
2024-02-06 07:47:16 -0300

    Final 06/02/2024

--- app.js
+++ app.js
@@ -21,15 +21,18 @@ Transformando a codificação anterior em função, para facilitar e reduzir as 
 
 /* Inicio do código - (importante que as variáveis fiquem no inicio do código) */
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1
 
 function exibirTextoTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 };
-
 /* Executando a função */
-exibirTextoTela('h1', 'Jogo do número secreto');
-exibirTextoTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial() {
+    exibirTextoTela('h1', 'Jogo do número secreto');
+    exibirTextoTela('p', 'Escolha um número entre 1 e 10');
+};
+exibirMensagemInicial();
 
 /* Habilitar botão da linha 27: <button onclick="verificarChute()" class="container__botao">Chutar</button>
 
@@ -38,15 +41,40 @@ function verificarChute() {
     /*Criando a interação do usuário*/
     let chute = document.querySelector('input').value;
     /* Uso de boolean - verdadeiro ou falso */
-    console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto){
+        exibirTextoTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas':'tentativa';
+        let mensagemTentativa = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoTela('p', mensagemTentativa);
+        /*Para ativar o botar da linha 28 <button onclick="" id="reiniciar" class="container__botao" disabled>Novo jogo</button>; e para reuniar o jogo, colocalmos uma função na variável onclick */
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else { 
+        if (chute > numeroSecreto) {
+        exibirTextoTela('p', 'O número secreto é menor!');
+        } else {
+            exibirTextoTela('p', 'O número secreto é maior!');
+        };
+        tentativas++;
+        limparCampo();
+    };
 };
-
 /*Necessário colocar return, para que essa função guarde um número dentro da variável criada no inicio deste código*/
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 };
-
 /* input = entrada do usurário
 Verificamos a linha 25 do código HTML, onde escrito input do tipo número de 1 a 10.: 
 <input type="number" min="1" max="10" class="container__input">
 */
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+};
+/* Dar funcionalidade ao botão Novo jogo, criando uma funão, com os comendos que devem ser executados, para que ele funcione */
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+};
